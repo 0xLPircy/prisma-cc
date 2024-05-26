304df43d684fbdb35c49e5e7b74ea400afd388bb
L-Pircy <taveesha.agarwal@gmail.com>
2024-05-26 12:06:42 +0530

    quering and prisma studio gui

--- index.ts
+++ index.ts
@@ -4,6 +4,77 @@ const prisma = new PrismaClient();
 
 async function main() {
   // prisma queries
+  ////   CREATE user
+  //   const user = await prisma.user.create({
+  //     data: {
+  //       name: "Esha",
+  //       email: "esh3ABC@gmail.com",
+  //     },
+  //   });
+  //// GET all users
+  const users = await prisma.user.findMany({
+    include: {
+      articles: true,
+    },
+  });
+  console.log(users, "\n");
+  ////   CREATE article and ASSOCIATE with user
+  //   const article = await prisma.article.create({
+  //     data: {
+  //       title: "Esha2 first article",
+  //       body: "lorem",
+  //       author: {
+  //         connect: {
+  //           id: 1,
+  //         },
+  //       },
+  //     },
+  //   });
+  //// GET ALL articles
+  //   const articles = await prisma.article.findMany();
+  // console.log(articles);
+  //// CREATE user AND article AND ASSSOCIATE them
+  //   const user = await prisma.user.create({
+  //     data: {
+  //       name: "Grace",
+  //       email: "grace123@gmail.com",
+  //       articles: {
+  //         create: {
+  //           title: "grace ka article",
+  //           body: "body de grace article",
+  //         },
+  //       },
+  //     },
+  //   });
+
+  // LOOP over author 1's articles
+  //   users.forEach((user) => {
+  //     console.log(`User: ${user.name}, \nEmail: ${user.email}`);
+  //     console.log(`\nArticles:`);
+  //     user.articles.forEach((article) => {
+  //       console.log(`\n\t- Title: ${article.title}, \n\t- Body: ${article.body}`);
+  //     });
+  //     console.log("\n");
+  //   });
+
+  //   UPDATE
+  //   const user = await prisma.user.update({
+  //     where: {
+  //       id: 1,
+  //     },
+  //     data: {
+  //       name: "Esha ABC",
+  //     },
+  //   });
+  //   console.log(user);
+
+  //   DELETE remove ARTICLE
+  //   const article = await prisma.article.delete({
+  //     where: {
+  //       id: 2,
+  //     },
+  //   });
+  //   console.log(article);
 }
 
 main()
